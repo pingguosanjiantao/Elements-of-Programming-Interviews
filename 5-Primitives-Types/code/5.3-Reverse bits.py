86b43b0a3a0f24a024ea0a6a1954c0344d0edbeb
yangchengmin <yangchengmin@baidu.com>
2018-01-15 15:51:35 +0800

    modify 5.3 code

--- 5-Primitives-Types/code/5.3-Reverse bits.py
+++ 5-Primitives-Types/code/5.3-Reverse bits.py
@@ -1,19 +1,20 @@
 # brute-force
 def reverseBits(x):
-    i = 0
-    while (x >> i) != 0:
+    for i in range(4):
         mask = (1 << i)
         if x & mask == 0:
             x |= mask
         else:
             x &= ~mask
-        i += 1
     return x
 
 
 # using a cache
 def reverseBits_cache(x):
-    precomputedReverse = [0b0, 0b0, 0b1, 0b0, 0b11, 0b10, 0b1, 0b0, 0b111, 0b110, 0b101, 0b100, 0b11, 0b10, 0b1, 0b0]
+    precomputedReverse = [0b0000,0b1000,0b0100,0b1100,
+                          0b0010,0b1010,0b0110,0b1110,
+                          0b0001,0b1001,0b0101,0b1101,
+                          0b0011,0b1011,0b0111,0b1111]
     wordSize = 4
     bitMask = 0xF
     ret = 0
@@ -21,4 +22,4 @@ def reverseBits_cache(x):
         ret |= precomputedReverse[((x >> i * wordSize) & bitMask)] << ((32 / wordSize - 1 - i) * wordSize)
     return ret
 
-print bin(reverseBits_cache(0b1111000011110000))
+print bin(reverseBits_cache(0b1000))
