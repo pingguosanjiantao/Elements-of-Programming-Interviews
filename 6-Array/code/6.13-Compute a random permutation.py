fa463b52cb534aae336a99ff2c8b84b1264192d4
yangchengmin <yangchengmin@baidu.com>
2018-01-18 10:24:24 +0800

    add 6.13

---
+++ 6-Array/code/6.13-Compute a random permutation.py
@@ -0,0 +1,15 @@
+
+import random
+
+# this method is copies from 6.11
+def randomSampling(k, nums):
+    for i in range(k):
+        idx = i + random.randint(0, len(nums) - 1 - i)
+        nums[idx], nums[i] = nums[i], nums[idx]
+    return nums[:k]
+
+def computeRandomPermutation(k):
+    ret = [i for i in range(k)]
+    return randomSampling(len(ret), ret)
+
+print computeRandomPermutation(5)
