3ce6dfd30c802a145937aa47687b07205357e66e
yangchengmin <yangchengmin@baidu.com>
2018-01-19 15:39:49 +0800

    add 7.2

---
+++ 7-Strings/code/7.2-Base Conversion.py
@@ -0,0 +1,31 @@
+def convertBase(s, base):
+    if s is None or len(s) == 0:
+        return None
+    isNegative = True if s[0] == '-' else False
+    if isNegative:
+        s = s[1:]
+    ret = 0
+    for i in range(len(s)):
+        digit = ord(s[i]) - ord('0')
+        ret *= 10
+        ret += digit
+
+    retBased = constructFromBase(ret, base)
+    if isNegative:
+        retBased = '-' + retBased
+    return retBased
+
+
+# Assume that input integer is positive
+def constructFromBase(num, base):
+    ret = ''
+    while num > 0:
+        digit = num % base
+        num /= base
+        ret = ret + str(digit)
+    return '0' if len(ret) == 0 else ret
+
+
+print constructFromBase(0, 6)
+
+print convertBase('-28', 6)
