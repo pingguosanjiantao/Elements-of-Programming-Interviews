38a3e60bef5b32952d47daea2428199b0502db89
pingguosanjiantao <yangchengmin12@mails.ucas.edu.cn>
2018-01-24 23:10:41 +0800

    add 10.1

---
+++ 10-Binary-Trees/code/10.1-Test a binary tree is height-balanced.py
@@ -0,0 +1,16 @@
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+# optimal solution
+def isBalanced(root):
+    def doIsBalanced(root):
+        if root is None:
+            return 0
+        hl, hr = doIsBalanced(root.left), doIsBalanced(root.right)
+        if hl < 0 or hr < 0 or abs(hl - hr) > 1:
+            return -1
+
+    return doIsBalanced(root) >= 0
