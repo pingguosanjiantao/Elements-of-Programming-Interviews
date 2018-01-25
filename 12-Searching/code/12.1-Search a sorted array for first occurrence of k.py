af5a147bf2c001cf25086335045fbd883f6b7ffd
yangchengmin <yangchengmin@baidu.com>
2018-01-25 17:40:27 +0800

    add 12.1

---
+++ 12-Searching/code/12.1-Search a sorted array for first occurrence of k.py
@@ -0,0 +1,15 @@
+def searchFisrt(nums, key):
+    left, right = 0, len(nums) - 1
+    ret = None
+    while left <= right:
+        mid = (left + right) / 2
+        if nums[mid] == key:
+            ret = mid
+            right -= 1
+        elif nums[mid] > key:
+            right = mid - 1
+        else:
+            left = mid + 1
+    return ret
+
+print searchFisrt([-14, -10, 2, 108, 108, 243, 285, 285, 401], 285)
