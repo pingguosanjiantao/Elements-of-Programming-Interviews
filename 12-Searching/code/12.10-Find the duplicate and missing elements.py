04798e5734f3ffb6a5d5beddbd556fe2ad89a1a6
yangchengmin <yangchengmin@baidu.com>
2018-01-26 10:39:33 +0800

    optimize 12.10

--- 12-Searching/code/12.10-Find the duplicate and missing elements.py
+++ 12-Searching/code/12.10-Find the duplicate and missing elements.py
@@ -1,19 +1,22 @@
+# optimal solution
 def findDuplicateMissing(nums):
     missXORDup = 0
     length = len(nums)
     for i in range(length):
         missXORDup ^= (i ^ nums[i])
     diffBit = missXORDup & (~(missXORDup - 1))
-    missOrDup = 0
+    miss, dup = 0, 0
     for i in range(length):
         if (i & diffBit) != 0:
-            missOrDup ^= i
+            miss ^= i
+        else:
+            dup ^= i
         if (nums[i] & diffBit) != 0:
-            missOrDup ^= nums[i]
-    for ele in nums:
-        if ele == missOrDup:
-            return [missOrDup, missOrDup ^ missXORDup]
-    return [missOrDup ^ missXORDup, missOrDup]
+            miss ^= nums[i]
+        else:
+            dup ^= nums[i]
+
+    return [miss, dup]
 
 
 print findDuplicateMissing([5, 3, 0, 3, 1, 2])
