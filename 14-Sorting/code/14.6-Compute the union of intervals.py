e1e6df2f66861d76a085f67c8e2404836d17d48a
yangchengmin <yangchengmin@baidu.com>
2018-02-01 19:01:45 +0800

    add 14.6

---
+++ 14-Sorting/code/14.6-Compute the union of intervals.py
@@ -0,0 +1,18 @@
+def unionOfIntervals(intervals):
+    if len(intervals) == 0:
+        return []
+    intervals.sort(cmp=lambda x, y: x[0] - y[0])
+    ret = []
+    pre = intervals[0]
+    for i in range(1, len(intervals)):
+        cur = intervals[i]
+        if cur[0] <= pre[1]:
+            pre[1] = cur[1] if cur[1] > pre[1] else cur[0]
+        else:
+            ret += [pre]
+            pre = cur
+    ret += [pre]
+    return ret
+
+
+print unionOfIntervals([[0, 3], [2, 4], [9, 11], [12, 14], [13, 15], [15, 16]])
