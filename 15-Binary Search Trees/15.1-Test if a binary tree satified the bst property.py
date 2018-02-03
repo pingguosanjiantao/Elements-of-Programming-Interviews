18e97c795dfb88347c308aa9c1a7b2c31f0aa937
yangchengmin <yangchengmin@baidu.com>
2018-02-03 20:45:08 +0800

    add iteration version for 15.1

--- 15-Binary Search Trees/15.1-Test if a binary tree satified the bst property.py
+++ 15-Binary Search Trees/15.1-Test if a binary tree satified the bst property.py
@@ -1,3 +1,4 @@
+# assume root is not None
 def isBST(root):
     def doIsBST(root, lower, upper):
         if root is None:
@@ -8,3 +9,17 @@ def isBST(root):
             return doIsBST(root.left, lower, root.val) and doIsBST(root.right, root.val, upper)
 
     return doIsBST(root, float('-inf'), float('inf'))
+
+
+# iteration version
+def isBSTIteration(root):
+    queue = [[root, float('-inf'), float('inf')]]
+    while len(queue) > 0:
+        [cur, lower, upper] = queue.pop(0)
+        if cur.val < lower or cur.val > upper:
+            return False
+        if cur.left is not None:
+            queue += [[cur.left, lower, cur.val]]
+        if cur.right is not None:
+            queue += [[cur.right, root.val, upper]]
+    return Ture
