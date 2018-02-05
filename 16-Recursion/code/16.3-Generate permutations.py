6c5806ba3a4b311934d60efa00893439faecb693
yangchengmin <yangchengmin@baidu.com>
2018-02-05 15:42:31 +0800

    add 16.3

---
+++ 16-Recursion/code/16.3-Generate permutations.py
@@ -0,0 +1,18 @@
+# copied from 6.9
+def permute(s):
+    ret = []
+
+    def doPermute(left, right):
+        if left == right:
+            ret.append(s[:])
+        else:
+            for i in range(left, right + 1):
+                s[left], s[i] = s[i], s[left]
+                doPermute(left + 1, right)
+                s[left], s[i] = s[i], s[left]
+
+    doPermute(0, len(s) - 1)
+    return ret
+
+
+print permute(['a', 'b', 'c'])
