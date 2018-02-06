d10e92f3c32b00d741565a406e0a48ee1dad18db
yangchengmin <yangchengmin@baidu.com>
2018-02-06 20:17:15 +0800

    update 16.2

--- 16-Recursion/code/16.2-Generate all nonattacking placements of n-queens.py
+++ 16-Recursion/code/16.2-Generate all nonattacking placements of n-queens.py
@@ -1,17 +1,20 @@
 # optimal solution
-def queen(A, curRow=0):
-    if curRow == len(A):
+def queen(A, cur=0):
+    if cur == len(A):
         print A
     else:
         for col in range(len(A)):
-            A[curRow], flag = col, True
-            for row in range(curRow):
-                # not equal and not diagonal, the diff in horizontal not equals to the diff in vertical
-                if A[row] == col or abs(col - A[row]) == curRow - row:
-                    flag = False
-                    break
-            if flag:
-                queen(A, curRow + 1)
+            A[cur] = col
+            if isValid(A, cur, col):
+                queen(A, cur + 1)
+
+
+def isValid(A, cur, col):
+    for pre in range(cur):
+        # not equal and not diagonal, the diff in horizontal not equals to the diff in vertical
+        if A[pre] == col or abs(col - A[pre]) == cur - pre:
+            return False
+    return True
 
 
 queen([None] * 4)
