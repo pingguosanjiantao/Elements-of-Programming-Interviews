d007c7d35e8e274ba7bed3305b8ed7180ccb4ce4
yangchengmin <yangchengmin@baidu.com>
2018-02-06 23:15:21 +0800

    update and add an optimal solution

--- 16-Recursion/code/16.4-Generate the power set.py
+++ 16-Recursion/code/16.4-Generate the power set.py
@@ -2,15 +2,30 @@ def getPowerSet(nums):
     def doGetPowerSet(nums, begin, selected, ret):
         if begin == len(nums):
             ret += [selected[:]]
-        else:
-            selected += [nums[begin]]
-            doGetPowerSet(nums, begin + 1, selected, ret)
-            selected.pop()
-            doGetPowerSet(nums, begin + 1, selected, ret)
+            return
+        selected += [nums[begin]]
+        doGetPowerSet(nums, begin + 1, selected, ret)
+        selected.pop()
+        doGetPowerSet(nums, begin + 1, selected, ret)
 
     ret = []
     doGetPowerSet(nums, 0, [], ret)
     return ret
 
 
-print getPowerSet([0, 1, 2])
+# optimal solution using bit
+def getSubset(nums):
+    length = 2 ** len(nums)
+    ret = []
+    for i in range(length):
+        cur = []
+        for j in range(length):
+            if (1 << j) & i != 0:
+                cur += [nums[j]]
+        ret += [cur]
+    return ret
+
+
+# print getPowerSet([0, 1, 2, 3])
+
+print getSubset([0, 1, 2, 3])
