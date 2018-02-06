2483202f8d7f74a16f17f498b2c3444a1dc24d02
yangchengmin <yangchengmin@baidu.com>
2018-02-06 23:44:52 +0800

    format

--- 16-Recursion/code/16.6-Generate strings of matched parens.py
+++ 16-Recursion/code/16.6-Generate strings of matched parens.py
@@ -1,15 +1,16 @@
 def genBalancedParentheses(n):
     def doGenBalancedParentheses(left, right, cur, ret):
+        print cur
         if left == 0 and right == 0:
             ret += [cur[:]]
-        else:
-            if left > 0:
-                doGenBalancedParentheses(left - 1, right, cur + '(', ret)
-            if left < right:
-                doGenBalancedParentheses(left, right - 1, cur + ')', ret)
+            return
+        if left > 0:
+            doGenBalancedParentheses(left - 1, right, cur + '(', ret)
+        if left < right:
+            doGenBalancedParentheses(left, right - 1, cur + ')', ret)
 
     ret = []
     doGenBalancedParentheses(n, n, '', ret)
     return ret
 
-print genBalancedParentheses(2)
+print genBalancedParentheses(3)
