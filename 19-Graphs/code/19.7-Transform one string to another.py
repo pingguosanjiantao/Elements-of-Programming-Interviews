ed7e74743bf0d5cbe4561d7524b1e9fc4e7a4834
pingguosanjiantao <yangchengmin12@mails.ucas.edu.cn>
2018-02-12 22:06:09 +0800

    add 19.7

---
+++ 19-Graphs/code/19.7-Transform one string to another.py
@@ -0,0 +1,21 @@
+def transformString(dictionary, s, t):
+    q = []
+    dictionary.remove(s)
+    q.append([s, 0])
+    while len(q) > 0:
+        [curStr, dis] = q.pop(0)
+        if curStr == t:
+            return dis
+        for i in range(len(curStr)):
+            strStart = curStr[:i]
+            strEnd = curStr[i + 1:]
+            for j in range(26):
+                modStr = strStart + chr(ord('a') + j) + strEnd
+                if modStr in dictionary:
+                    dictionary.remove(modStr)
+                    q.append([modStr, dis + 1])
+    return -1
+
+
+dictionary = ['bat', 'cot', 'dog', 'dag', 'dot', 'cat']
+print transformString(dictionary, 'cot', 'dot')
