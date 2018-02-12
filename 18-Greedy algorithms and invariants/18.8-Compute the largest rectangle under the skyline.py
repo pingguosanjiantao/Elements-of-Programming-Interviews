d6d7bd38375c6c51138b7a0cac3e9ac697e78990
yangchengmin <yangchengmin@baidu.com>
2018-02-12 15:55:27 +0800

    add 18.8

---
+++ 18-Greedy algorithms and invariants/18.8-Compute the largest rectangle under the skyline.py
@@ -0,0 +1,20 @@
+# amazing problem, optimal solution
+# variant of 18.7
+def getLargestRectangle(nums):
+    ret = 0
+    stack = []  # store the idx of stepping up
+    for i in range(len(nums) + 1):
+        # update the idx of same value
+        if len(stack) > 0 and i < len(nums) and nums[i] == nums[stack[-1]]:
+            stack[-1] = i
+        # maximum the rectangle
+        while len(stack) > 0 and (i == len(nums) or nums[i] < nums[stack[-1]]):
+            height = nums[stack.pop()]
+            head = 0 if len(stack) == 0 else stack[-1] + 1
+            width = i - head
+            ret = max(ret, height * width)
+        stack += [i]
+    return ret
+
+
+print getLargestRectangle([1, 4, 2, 5, 6])
