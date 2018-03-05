77ef80e0b98f7325c1fd60ee117fd385b0332779
yangchengmin <yangchengmin@baidu.com>
2018-03-05 17:56:56 +0800

    add binary search in cyclically array

--- 12-Searching/code/12.3Search a cyclically sorted array.py
+++ 12-Searching/code/12.3Search a cyclically sorted array.py
@@ -8,5 +8,24 @@ def searchSmallest(nums):
             right = mid
     return nums[left]
 
+def searching(nums, target):
+    left, right = 0, len(nums)
+    while left < right:
+        mid = (left + right) / 2
+        if target == nums[mid]:
+            return mid
+        elif target < nums[mid]:
+            if target >= nums[left]:
+                right = mid
+            else:
+                left = mid + 1
+        else:
+            if target < nums[right - 1]:
+                left = mid + 1
+            else:
+                right = mid
+    return -1
+
+print searching([2], 3)
 
 print searchSmallest([3, 4, 5, 6, 1, 2])
