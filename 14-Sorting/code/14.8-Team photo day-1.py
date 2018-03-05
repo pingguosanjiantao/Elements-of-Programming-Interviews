26debf1c3aea52dd64d69a425507e272aa0fd959
pingguosanjiantao <yangchengmin12@mails.ucas.edu.cn>
2018-03-05 23:34:44 +0800

    fix bug

--- 14-Sorting/code/14.8-Team photo day-1.py
+++ 14-Sorting/code/14.8-Team photo day-1.py
@@ -2,6 +2,6 @@ def validPlacementExists(a, b):
     a.sort()
     b.sort()
     for i in range(min(len(a), len(b))):
-        if a[i] >= b[j]:
+        if a[i] >= b[i]:
             return False
     return True
