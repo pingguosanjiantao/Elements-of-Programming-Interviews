1f6a830f80e8c5341871d65ca5559a347bc055fd
pingguosanjiantao <yangchengmin12@mails.ucas.edu.cn>
2018-03-11 10:16:28 +0800

    add 18.7 for leetcode

--- 18-Greedy algorithms and invariants/18.7-Compute the maximum water trapped by a pair of vertical lines.py
+++ 18-Greedy algorithms and invariants/18.7-Compute the maximum water trapped by a pair of vertical lines.py
@@ -12,4 +12,24 @@ def getMaxTrappedWater(nums):
     return ret
 
 
+# leetcode 42
+def trap(nums):
+    if nums is None or len(nums) <= 2:
+        return 0
+
+    ret = 0
+    left, right = 0, len(nums) - 1
+    maxLeft, maxRight = 0, 0
+    while left < right:
+        maxLeft, maxRight = max(maxLeft, nums[left]), max(maxRight, nums[right])
+        if nums[left] < nums[right]:
+            ret += maxLeft - nums[left]
+            left += 1
+        else:
+            ret += maxRight - nums[right]
+            right -= 1
+    return ret
+
+
 print getMaxTrappedWater([1, 2, 1, 3, 4, 4, 5, 6, 2, 1, 3, 1, 3, 2, 1, 2, 4, 1])
+print trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1])
