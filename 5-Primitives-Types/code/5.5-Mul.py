650359a7605d763637a5531d574df9ec90219ede
pingguosanjiantao <yangchengmin12@mails.ucas.edu.cn>
2018-04-21 21:49:50 +0800

    fix bug

--- 5-Primitives-Types/code/5.5-Mul.py
+++ 5-Primitives-Types/code/5.5-Mul.py
@@ -2,7 +2,7 @@ def add(x, y):
     ret = x ^ y
     carry = x & y
     while carry != 0:
-        if (1 << 30) & carry  == 0 or (1 << 31) & carry == 0:
+        if (1 << 30) & carry  != 0 or (1 << 31) & carry != 0:
             print 'overflow'
             return None
         x, y = ret, carry << 1
