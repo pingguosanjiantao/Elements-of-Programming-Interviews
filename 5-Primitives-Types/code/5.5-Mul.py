6818824f4da1ce740962b958c2fb2d3447ccd470
pingguosanjiantao <yangchengmin12@mails.ucas.edu.cn>
2018-04-21 20:40:35 +0800

    add the overflow of add operator

--- 5-Primitives-Types/code/5.5-Mul.py
+++ 5-Primitives-Types/code/5.5-Mul.py
@@ -2,6 +2,9 @@ def add(x, y):
     ret = x ^ y
     carry = x & y
     while carry != 0:
+        if (1 << 30) & carry  == 0 or (1 << 31) & carry == 0:
+            print 'overflow'
+            return None
         x, y = ret, carry << 1
         ret = x ^ y
         carry = x & y
