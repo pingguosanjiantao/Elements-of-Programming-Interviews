7f25e6ae44ad3703e72e803e24bb8ae0f26e8f80
pingguosanjiantao <yangchengmin12@mails.ucas.edu.cn>
2018-05-04 22:11:24 +0800

    using stack to optimize the 14.6 problem

--- 14-Sorting/code/14.6-Compute the union of intervals.py
+++ 14-Sorting/code/14.6-Compute the union of intervals.py
@@ -2,17 +2,16 @@ def unionOfIntervals(intervals):
     if len(intervals) == 0:
         return []
     intervals.sort(cmp=lambda x, y: x[0] - y[0])
-    ret = []
-    pre = intervals[0]
+    stack = [intervals[0]]
     for i in range(1, len(intervals)):
-        cur = intervals[i]
-        if cur[0] <= pre[1]:
-            pre[1] = cur[1] if cur[1] > pre[1] else pre[1]
+        peek, cur = stack[-1], intervals[i]
+        if cur[0] > peek[1]:
+            stack.append(cur)
         else:
-            ret += [pre]
-            pre = cur
-    ret += [pre]
-    return ret
+            if cur[1] >= peek[1]:
+                stack.pop()
+                stack.append([peek[0], cur[1]])
+    return stack
 
 
 print unionOfIntervals([[0, 3], [2, 4], [9, 11], [12, 14], [13, 15], [15, 16]])
