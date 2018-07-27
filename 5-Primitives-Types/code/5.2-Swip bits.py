676a7b476d523c9e28f0469d795aae9d9d183f3a
pingguosanjiantao <yangchengmin12@mails.ucas.edu.cn>
2018-07-27 18:04:28 +0800

    fix bug

--- 5-Primitives-Types/code/5.2-Swip bits.py
+++ 5-Primitives-Types/code/5.2-Swip bits.py
@@ -1,7 +1,7 @@
 # bit-fiddling idioms
 def swipBits_bit_fiddling(x, i, j):
     # extract the i-th and j-th bits, and see if they differ
-    if ((x >> i) & 1) != ((j >> j) & 1):
+    if ((x >> i) & 1) != ((x >> j) & 1):
         bitMask = (1 << i) | (1 << j)
         x ^= bitMask
     return x
